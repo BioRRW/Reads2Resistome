d34f3874191aed6de77b06581149339b8baca0be
Reed Woyda <49286465+BioRRW@users.noreply.github.com>
2021-02-03 11:33:10 -0700

    Create install.sh
    Added install script to download Singularity images. See Installation section for details.

---
+++ containers/install.sh
@@ -0,0 +1,11 @@
+singularity pull R2R_Main-0.0.2.simg library://biorrw/default/reads2resistome:sha256.03cdd22921c2adafd49790bc9fc7655dfa81ba4287950ab629986a507050df82
+
+singularity pull R2R_ABRICATE-0.0.1.simg library://biorrw/default/reads2resistome:sha256.0efeef9d0e051d42fc80e8e3edcb0ab45d69dbad836f0ac65533196d7b9fe4d9
+
+singularity pull R2R_Phigaro-0.0.1.simg library://biorrw/default/reads2resistome:sha256.7315e84ee4bfb8e5cb5bfe1aa76067a2cd6efc52e642b7d5e4a3f0a8fbc006d4
+
+singularity pull R2R_Prokka-0.0.2.simg library://biorrw/default/reads2resistome:sha256.82d2738e783f098c27bbc0604b75c2ec543469bc7397bfccb8f0a9748f2c8ef6
+
+singularity pull R2R_nanoplot-0.0.2.simg library://biorrw/default/reads2resistome:sha256.522aaa4b01b1bc7f4454cfe0c75ac6de069704d887bd16915383197c3352f08f
+
+singularity pull R2R_QUAST-0.0.2.simg library://biorrw/default/reads2resistome:sha256.a12c6d9fc0b6e7b89a2199454c5e7b1d817c82c8033ab664097d01664d44e553
